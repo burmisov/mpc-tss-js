d75e4f1e30c647d59ef9cb174b458993cfa62b30
Sergey Burmisov <skypath@gmail.com>
2023-11-22 23:28:19 +0100

    feat: pedersen: implement commit and verify

--- src/lib/pedersen.ts
+++ src/lib/pedersen.ts
@@ -1,4 +1,4 @@
-import { gcd } from "bigint-crypto-utils";
+import { gcd, modPow, modMultiply } from "bigint-crypto-utils";
 
 export type PedersenParameters = {
   n: bigint;
@@ -49,4 +49,36 @@ export const pedersenValidateParameters = (
   if (gcd(t, n) !== 1n) {
     throw new Error('INVALID_PEDERSEN_PARAMETERS: t must be coprime to n');
   }
+}
+
+export const pedersenCommit = (
+  parameters: PedersenParameters,
+  x: bigint,
+  y: bigint,
+): bigint => {
+  const sx = modPow(parameters.s, x, parameters.n);
+  const ty = modPow(parameters.t, y, parameters.n);
+  return modMultiply([sx, ty], parameters.n);
+};
+
+export const pedersenVerify = (
+  parameters: PedersenParameters,
+  a: bigint, b: bigint, e: bigint,
+  S: bigint, T: bigint,
+): boolean => {
+  try {
+    pedersenValidateParameters({ n: parameters.n, s: S, t: T });
+  } catch (error) {
+    // TODO: check error type
+    return false;
+  }
+
+  const sa = modPow(parameters.s, a, parameters.n);
+  const tb = modPow(parameters.t, b, parameters.n);
+  const lhs = modMultiply([sa, tb], parameters.n);
+
+  const te = modPow(T, e, parameters.n);
+  const rhs = modMultiply([te, S], parameters.n);
+
+  return lhs === rhs;
 }
