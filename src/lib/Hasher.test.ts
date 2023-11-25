63b8c25009fcf8a7ab2e4a1a8b0e9305f448720b
Sergey Burmisov <skypath@gmail.com>
2023-11-25 18:21:43 +0100

    feat: implement a v0 hasher helper

---
+++ src/lib/Hasher.test.ts
@@ -0,0 +1,105 @@
+import { beforeEach, describe, it } from "node:test";
+import assert from 'assert/strict';
+import { Hasher } from "./Hasher.js";
+import { AffinePoint } from "./common.types.js";
+import { secp256k1 } from "@noble/curves/secp256k1";
+import { paillierGeneratePedersen, paillierSecretKeyFromPrimes } from "./paillier.js";
+
+describe('Hasher', () => {
+  const p = 179592502110335963336347735108907147317760904272746519157588428198851642173043932077383231024080457777437444199308940940528740158020956955835017958704625931695110457545843284994471316520797998498062474296013358438785968440081020607611888287234488233606613994066898948321434201732737366068220153564935475802567n;
+  const q = 144651337722999591357894368476987413731327694772730408677878934803626218325763401733049627551150267745019646164141178748986827450041894571742897062718616997949877925740444144291875968298065299373438319317040746398994377200405476019627025944607850551945311780131978961657839712750089596117856255513589953855963n;
+  const paillierSecretKey = paillierSecretKeyFromPrimes(p, q);
+
+  let hasher: Hasher;
+
+  beforeEach(() => {
+    hasher = new Hasher();
+  });
+
+  it('hashes a string', () => {
+    const data: string = 'hello world';
+    const hash = hasher.update(data).digestBytes();
+    assert.equal(hash.length, 32);
+  });
+
+  it('supports bigint', () => {
+    const data: string = 'hello world';
+    const hash = hasher.update(data).digestBigint();
+    assert.equal(typeof hash, 'bigint');
+  });
+
+  it('hashes a Uint8Array', () => {
+    const data = new Uint8Array([1, 2, 3, 4, 5]);
+    const hash = hasher.update(data).digestBigint();
+    const originalHash = new Hasher().update(data).digestBigint();
+    assert.equal(hash, originalHash);
+  });
+
+  it('prevents reuse', () => {
+    const data: string = 'hello world';
+    hasher.update(data).digestBytes();
+    assert.throws(() => {
+      hasher.digestBytes();
+    });
+    assert.throws(() => {
+      hasher.update('foo');
+    });
+  });
+
+  it('hashes an AffinePoint', () => {
+    const data: AffinePoint = {
+      x: 1n,
+      y: 2n,
+    };
+    const hash = hasher.update(data).digestBigint();
+    const originalHash = new Hasher().update(data.x).update(data.y).digestBigint();
+    assert.equal(hash, originalHash);
+  });
+
+  it('hashes a ProjectivePoint', () => {
+    const data = secp256k1.ProjectivePoint.BASE;
+    const hash = hasher.update(data).digestBigint();
+    const originalHash = new Hasher().update(data.toRawBytes()).digestBigint();
+    assert.equal(hash, originalHash);
+  });
+
+  it('hashes multiple things', () => {
+    const data = [
+      'hello world',
+      1n,
+      new Uint8Array([1, 2, 3, 4, 5]),
+      secp256k1.ProjectivePoint.BASE,
+    ];
+    const hash = hasher.updateMulti(data).digestBigint();
+    const originalHash = new Hasher()
+      .update(data[0])
+      .update(data[1])
+      .update(data[2])
+      .update(secp256k1.ProjectivePoint.BASE.toRawBytes())
+      .digestBigint();
+    assert.equal(hash, originalHash);
+  });
+
+  it('hashes a Paillier public key', () => {
+    const paillierPublicKey = paillierSecretKey.publicKey;
+
+    const hash = hasher.update(paillierPublicKey).digestBigint();
+    const originalHash = new Hasher()
+      .update(paillierPublicKey.n)
+      .update(paillierPublicKey.nSquared)
+      .update(paillierPublicKey.nPlusOne)
+      .digestBigint();
+    assert.equal(hash, originalHash);
+  });
+
+  it('hashes Pedersen parameters', () => {
+    const { pedersen } = paillierGeneratePedersen(paillierSecretKey);
+    const hash = hasher.update(pedersen).digestBigint();
+    const originalHash = new Hasher()
+      .update(pedersen.n)
+      .update(pedersen.s)
+      .update(pedersen.t)
+      .digestBigint();
+    assert.equal(hash, originalHash);
+  });
+});
---
+++ src/lib/Hasher.ts
@@ -0,0 +1,111 @@
+import { bytesToNumberBE } from "@noble/curves/abstract/utils";
+import { blake3 } from "@noble/hashes/blake3";
+import { Input, hexToBytes } from "@noble/hashes/utils";
+import { ProjectivePoint, AffinePoint } from "./common.types.js";
+import { PaillierPublicKey } from "./paillier.js";
+import { PedersenParameters } from "./pedersen.js";
+
+type IngestableBasic = Uint8Array | string | bigint;
+type Ingestable = IngestableBasic | ProjectivePoint | AffinePoint |
+  PaillierPublicKey | PedersenParameters;
+
+export class Hasher {
+  private hash: ReturnType<typeof blake3.create>;
+  private used: boolean = false;
+
+  constructor() {
+    this.hash = blake3.create({}); // TODO: pass params?
+  }
+
+  private checkUsed() {
+    if (this.used) {
+      throw new Error('Hasher already used');
+    }
+  }
+
+  public digestBytes(): Uint8Array {
+    this.checkUsed();
+    this.used = true;
+    return this.hash.digest();
+  }
+
+  public digestBigint(): bigint {
+    this.checkUsed();
+    this.used = true;
+    return bytesToNumberBE(this.hash.digest());
+  }
+
+  private updateBasic(data: IngestableBasic): Hasher {
+    this.checkUsed();
+    let buf: Input;
+    if (data instanceof Uint8Array) {
+      buf = data;
+    } else if (typeof data === 'string') {
+      buf = data;
+    } else if (typeof data === 'bigint') {
+      let hex = data.toString(16);
+      if (hex.length % 2 === 1) {
+        hex = `0${hex}`;
+      }
+      buf = hexToBytes(hex);
+    } else {
+      throw new Error('Unsupported data type', data);
+    }
+    this.hash.update(buf);
+    return this;
+  }
+
+  // TODO: make identifiable objects instead?..
+  public update(data: Ingestable): Hasher {
+    this.checkUsed();
+    let buf: Array<IngestableBasic> = [];
+    if (
+      data instanceof Uint8Array ||
+      typeof data === 'string' ||
+      typeof data === 'bigint'
+    ) {
+      buf.push(data);
+    } else if (typeof (data as PaillierPublicKey).n === 'bigint' &&
+      typeof (data as PaillierPublicKey).nSquared === 'bigint' &&
+      typeof (data as PaillierPublicKey).nPlusOne === 'bigint' &&
+      Object.keys(data).length === 3
+    ) {
+      buf.push((data as PaillierPublicKey).n);
+      buf.push((data as PaillierPublicKey).nSquared);
+      buf.push((data as PaillierPublicKey).nPlusOne);
+    } else if (
+      typeof (data as PedersenParameters).n === 'bigint' &&
+      typeof (data as PedersenParameters).s === 'bigint' &&
+      typeof (data as PedersenParameters).t === 'bigint' &&
+      Object.keys(data).length === 3
+    ) {
+      buf.push((data as PedersenParameters).n);
+      buf.push((data as PedersenParameters).s);
+      buf.push((data as PedersenParameters).t);
+    } else if (
+      typeof (data as AffinePoint).x === 'bigint' &&
+      typeof (data as AffinePoint).y === 'bigint'
+    ) {
+      if (typeof (data as ProjectivePoint).toRawBytes === 'function') {
+        buf.push((data as ProjectivePoint).toRawBytes());
+      } else {
+        buf.push((data as AffinePoint).x);
+        buf.push((data as AffinePoint).y);
+      }
+    } else {
+      throw new Error(`Unsupported data type: ${data}`);
+    }
+    for (let i = 0; i < buf.length; i += 1) {
+      this.updateBasic(buf[i]);
+    }
+    return this;
+  }
+
+  public updateMulti(data: Array<Ingestable>): Hasher {
+    this.checkUsed();
+    for (let i = 0; i < data.length; i += 1) {
+      this.update(data[i]);
+    }
+    return this;
+  }
+}
