84abec5a95e5c897224d85a0a1288944bccd08a1
Sergey Burmisov <skypath@gmail.com>
2023-11-26 19:13:46 +0100

    feat: implement mta

---
+++ src/lib/mta.test.ts
@@ -0,0 +1,162 @@
+import { describe, it } from "node:test";
+import assert from "node:assert/strict";
+
+import { secp256k1 } from "@noble/curves/secp256k1";
+import { randBetween } from "bigint-crypto-utils";
+
+import {
+  PaillierPublicKey, PaillierSecretKey,
+  paillierDecrypt, paillierEncrypt, paillierGeneratePedersen,
+  paillierSecretKeyFromPrimes, validatePaillierPrime,
+} from "./paillier.js";
+import { PedersenParameters } from "./pedersen.js";
+import Fn from "./Fn.js";
+import { mtaProveAffG, mtaProveAffP } from "./mta.js";
+import { ZkAffgPublic, zkAffgVerifyProof } from "./zk/affg.js";
+import { ZkAffpPublic, zkAffpVerifyProof } from "./zk/affp.js";
+
+describe('mta', async () => {
+  let proverPaillierSecretKey: PaillierSecretKey;
+  let proverPaillierPublicKey: PaillierPublicKey;
+  let verifierPaillierSecretKey: PaillierSecretKey;
+  let verifierPaillierPublicKey: PaillierPublicKey;
+  let verifierPedersen: PedersenParameters;
+
+  {
+    const p = 167495246782569910107862669897619243075835098207853220101403982609959943903743345646375788362909715211900141652671436007095125366426921092792005772260273820630537327116191279465491203987638225198595642843240698088777581339844121640128731583440776342448939839731522446469995390448902946229002446098530004835239n;
+    const q = 178784929586423449637890491161861655617854412540709400421874212815293580828404739498291345696103341491924297140261396221041987821086550770172144419152711267591283272834659746554330603868249176073673884285246036132552905332762099384955889000396765335249879642433930458968871576233738650973235318810378637560383n;
+    await validatePaillierPrime(p);
+    await validatePaillierPrime(q);
+    proverPaillierSecretKey = paillierSecretKeyFromPrimes(p, q);
+    proverPaillierPublicKey = proverPaillierSecretKey.publicKey;
+  }
+
+  {
+    const p = 179592502110335963336347735108907147317760904272746519157588428198851642173043932077383231024080457777437444199308940940528740158020956955835017958704625931695110457545843284994471316520797998498062474296013358438785968440081020607611888287234488233606613994066898948321434201732737366068220153564935475802567n;
+    const q = 144651337722999591357894368476987413731327694772730408677878934803626218325763401733049627551150267745019646164141178748986827450041894571742897062718616997949877925740444144291875968298065299373438319317040746398994377200405476019627025944607850551945311780131978961657839712750089596117856255513589953855963n;
+    await validatePaillierPrime(p);
+    await validatePaillierPrime(q);
+    verifierPaillierSecretKey = paillierSecretKeyFromPrimes(p, q);
+    verifierPaillierPublicKey = verifierPaillierSecretKey.publicKey;
+    const { pedersen } = paillierGeneratePedersen(verifierPaillierSecretKey);
+    verifierPedersen = pedersen;
+  }
+
+  const paillierI = proverPaillierPublicKey;
+  const paillierJ = verifierPaillierPublicKey;
+
+  const ski = proverPaillierSecretKey;
+  const skj = verifierPaillierSecretKey;
+  const ai = randBetween(Fn.N - 1n);
+  const aj = randBetween(Fn.N - 1n);
+
+  const bi = randBetween(Fn.N - 1n);
+  const bj = randBetween(Fn.N - 1n);
+
+  const { ciphertext: Bi } = paillierEncrypt(paillierI, bi);
+  const { ciphertext: Bj } = paillierEncrypt(paillierJ, bj);
+
+  const aibj = Fn.mul(ai, bj);
+  const ajbi = Fn.mul(aj, bi);
+  const c = Fn.add(aibj, ajbi);
+
+  const verifyMta = (
+    Di: bigint, // Ciphertext
+    Dj: bigint, // Ciphertext
+    betaI: bigint,
+    betaJ: bigint,
+  ) => {
+    const alphaI = paillierDecrypt(ski, Dj);
+    const alphaJ = paillierDecrypt(skj, Di);
+
+    const gammaI = alphaI + betaI;
+    const gammaJ = alphaJ + betaJ;
+    const gamma = gammaI + gammaJ;
+    const gammaS = Fn.mod(gamma);
+
+    assert.strictEqual(c, gammaS, "a•b should be equal to α + β");
+  }
+
+  it('proves AffG', () => {
+    const Ai = secp256k1.ProjectivePoint.BASE.multiply(ai).toAffine();
+    const Aj = secp256k1.ProjectivePoint.BASE.multiply(aj).toAffine();
+
+    const {
+      Beta: betaI, D: Di, F: Fi, proof: proofI,
+    } = mtaProveAffG(
+      ai, Ai, Bj, ski, paillierJ, verifierPedersen,
+    );
+    const {
+      Beta: betaJ, D: Dj, F: Fj, proof: proofJ,
+    } = mtaProveAffG(
+      aj, Aj, Bi, skj, paillierI, verifierPedersen,
+    );
+
+    const pubI: ZkAffgPublic = {
+      Kv: Bj,
+      Dv: Di,
+      Fp: Fi,
+      Xp: Ai,
+      prover: paillierI,
+      verifier: paillierJ,
+      aux: verifierPedersen,
+    };
+    const verifiedI = zkAffgVerifyProof(proofI, pubI);
+    assert.strictEqual(verifiedI, true, "Proof I verification failed");
+
+    const pubJ: ZkAffgPublic = {
+      Kv: Bi,
+      Dv: Dj,
+      Fp: Fj,
+      Xp: Aj,
+      prover: paillierJ,
+      verifier: paillierI,
+      aux: verifierPedersen,
+    };
+    const verifiedJ = zkAffgVerifyProof(proofJ, pubJ);
+    assert.strictEqual(verifiedJ, true, "Proof J verification failed");
+
+    verifyMta(Di, Dj, betaI, betaJ);
+  });
+
+  it('proves AffP', () => {
+    const { ciphertext: Ai, nonce: nonceI } = paillierEncrypt(ski.publicKey, ai);
+    const { ciphertext: Aj, nonce: nonceJ } = paillierEncrypt(skj.publicKey, aj);
+    const {
+      Beta: betaI, D: Di, F: Fi, proof: proofI,
+    } = mtaProveAffP(
+      ai, Ai, nonceI, Bj, ski, paillierJ, verifierPedersen,
+    );
+    const {
+      Beta: betaJ, D: Dj, F: Fj, proof: proofJ,
+    } = mtaProveAffP(
+      aj, Aj, nonceJ, Bi, skj, paillierI, verifierPedersen,
+    );
+
+    const pubI: ZkAffpPublic = {
+      Kv: Bj,
+      Dv: Di,
+      Fp: Fi,
+      Xp: Ai,
+      prover: paillierI,
+      verifier: paillierJ,
+      aux: verifierPedersen,
+    };
+    const verifiedI = zkAffpVerifyProof(proofI, pubI);
+    assert.strictEqual(verifiedI, true, "Proof I verification failed");
+
+    const pubJ: ZkAffpPublic = {
+      Kv: Bi,
+      Dv: Dj,
+      Fp: Fj,
+      Xp: Aj,
+      prover: paillierJ,
+      verifier: paillierI,
+      aux: verifierPedersen,
+    };
+    const verifiedJ = zkAffpVerifyProof(proofJ, pubJ);
+    assert.strictEqual(verifiedJ, true, "Proof J verification failed");
+
+    verifyMta(Di, Dj, betaI, betaJ);
+  });
+});
---
+++ src/lib/mta.ts
@@ -0,0 +1,119 @@
+import { AffinePoint } from "./common.types.js";
+import {
+  PaillierPublicKey, PaillierSecretKey,
+  paillierAdd, paillierEncrypt, paillierMultiply,
+} from "./paillier.js";
+import { PedersenParameters } from "./pedersen.js";
+import { sampleIntervalLprime } from "./sample.js";
+import {
+  ZkAffgPrivate, ZkAffgProof, ZkAffgPublic, zkAffgCreateProof,
+} from "./zk/affg.js";
+import {
+  ZkAffpPrivate, ZkAffpProof, ZkAffpPublic, zkAffpCreateProof,
+} from "./zk/affp.js";
+
+export const mtaProveAffG = (
+  senderSecretShare: bigint,
+  senderSecretSharePoint: AffinePoint,
+  receiverEncryptedShare: bigint, // Ciphertext
+  sender: PaillierSecretKey,
+  receiver: PaillierPublicKey,
+  verifier: PedersenParameters,
+): {
+  Beta: bigint,
+  D: bigint, // Ciphertext
+  F: bigint, // Ciphertext
+  proof: ZkAffgProof,
+} => {
+  const {
+    D, F, S, R, BetaNeg,
+  } = mtaNew(senderSecretShare, receiverEncryptedShare, sender, receiver);
+
+  const pub: ZkAffgPublic = {
+    Kv: receiverEncryptedShare,
+    Dv: D,
+    Fp: F,
+    Xp: senderSecretSharePoint,
+    prover: sender.publicKey,
+    verifier: receiver,
+    aux: verifier,
+  };
+  const priv: ZkAffgPrivate = {
+    X: senderSecretShare,
+    Y: BetaNeg,
+    S,
+    R,
+  };
+  const proof = zkAffgCreateProof(pub, priv);
+
+  const Beta = -BetaNeg;
+
+  return { Beta, D, F, proof };
+};
+
+export const mtaProveAffP = (
+  senderSecretShare: bigint,
+  senderEncryptedShare: bigint, // Ciphertext
+  senderEncryptedShareNonce: bigint,
+  receiverEncryptedShare: bigint, // Ciphertext
+  sender: PaillierSecretKey,
+  receiver: PaillierPublicKey,
+  verifier: PedersenParameters,
+): {
+  Beta: bigint,
+  D: bigint, // Ciphertext
+  F: bigint, // Ciphertext
+  proof: ZkAffpProof,
+} => {
+  const {
+    D, F, S, R, BetaNeg,
+  } = mtaNew(senderSecretShare, receiverEncryptedShare, sender, receiver);
+
+  const pub: ZkAffpPublic = {
+    Kv: receiverEncryptedShare,
+    Dv: D,
+    Fp: F,
+    Xp: senderEncryptedShare,
+    prover: sender.publicKey,
+    verifier: receiver,
+    aux: verifier,
+  };
+  const priv: ZkAffpPrivate = {
+    X: senderSecretShare,
+    Y: BetaNeg,
+    S,
+    Rx: senderEncryptedShareNonce,
+    R,
+  };
+  const proof = zkAffpCreateProof(pub, priv);
+
+  const Beta = -BetaNeg;
+
+  return { Beta, D, F, proof };
+};
+
+export const mtaNew = (
+  senderSecretShare: bigint,
+  receiverEncryptedShare: bigint, // Ciphertext
+  sender: PaillierSecretKey,
+  receiver: PaillierPublicKey,
+): {
+  D: bigint; // Ciphertext
+  F: bigint; // Ciphertext
+  S: bigint;
+  R: bigint;
+  BetaNeg: bigint;
+} => {
+  const BetaNeg = sampleIntervalLprime();
+
+  const { ciphertext: F, nonce: R } = paillierEncrypt(sender.publicKey, BetaNeg);
+
+  const { ciphertext: D_, nonce: S } = paillierEncrypt(receiver, BetaNeg);
+  const D = paillierAdd(
+    receiver,
+    D_,
+    paillierMultiply(receiver, receiverEncryptedShare, senderSecretShare),
+  );
+
+  return { D, F, S, R, BetaNeg };
+};
