0f598fd0059318f6e534c64b7b823ec0bdd37011
Sergey Burmisov <skypath@gmail.com>
2023-11-26 18:07:49 +0100

    feat: zk: implement affp

---
+++ src/lib/zk/affp.test.ts
@@ -0,0 +1,85 @@
+import { describe, it } from "node:test";
+import assert from "node:assert/strict";
+
+import {
+  PaillierPublicKey, PaillierSecretKey, paillierAdd, paillierEncrypt,
+  paillierGeneratePedersen, paillierMultiply, paillierSecretKeyFromPrimes,
+  validatePaillierPrime,
+} from "../paillier.js";
+import { PedersenParameters } from "../pedersen.js";
+import { sampleIntervalL } from "../sample.js";
+import {
+  ZkAffpPrivate, ZkAffpPublic, zkAffpCreateProof, zkAffpVerifyProof,
+} from "./affp.js";
+
+describe("zk/affp", () => {
+  it("create proof and verify", async () => {
+    let proverPaillierSecretKey: PaillierSecretKey;
+    let proverPaillierPublicKey: PaillierPublicKey;
+    let verifierPaillierSecretKey: PaillierSecretKey;
+    let verifierPaillierPublicKey: PaillierPublicKey;
+    let verifierPedersen: PedersenParameters;
+
+    {
+      const p = 167495246782569910107862669897619243075835098207853220101403982609959943903743345646375788362909715211900141652671436007095125366426921092792005772260273820630537327116191279465491203987638225198595642843240698088777581339844121640128731583440776342448939839731522446469995390448902946229002446098530004835239n;
+      const q = 178784929586423449637890491161861655617854412540709400421874212815293580828404739498291345696103341491924297140261396221041987821086550770172144419152711267591283272834659746554330603868249176073673884285246036132552905332762099384955889000396765335249879642433930458968871576233738650973235318810378637560383n;
+      await validatePaillierPrime(p);
+      await validatePaillierPrime(q);
+      proverPaillierSecretKey = paillierSecretKeyFromPrimes(p, q);
+      proverPaillierPublicKey = proverPaillierSecretKey.publicKey;
+    }
+
+    {
+      const p = 179592502110335963336347735108907147317760904272746519157588428198851642173043932077383231024080457777437444199308940940528740158020956955835017958704625931695110457545843284994471316520797998498062474296013358438785968440081020607611888287234488233606613994066898948321434201732737366068220153564935475802567n;
+      const q = 144651337722999591357894368476987413731327694772730408677878934803626218325763401733049627551150267745019646164141178748986827450041894571742897062718616997949877925740444144291875968298065299373438319317040746398994377200405476019627025944607850551945311780131978961657839712750089596117856255513589953855963n;
+      await validatePaillierPrime(p);
+      await validatePaillierPrime(q);
+      verifierPaillierSecretKey = paillierSecretKeyFromPrimes(p, q);
+      verifierPaillierPublicKey = verifierPaillierSecretKey.publicKey;
+      const { pedersen } = paillierGeneratePedersen(verifierPaillierSecretKey);
+      verifierPedersen = pedersen;
+    }
+
+    const c = 12n;
+    const { ciphertext: C } = paillierEncrypt(verifierPaillierPublicKey, c);
+
+    const x = sampleIntervalL();
+    const { ciphertext: X, nonce: rhoX } = paillierEncrypt(
+      proverPaillierPublicKey, x,
+    );
+
+    const y = sampleIntervalL();
+    const { ciphertext: Y, nonce: rhoY } = paillierEncrypt(
+      proverPaillierPublicKey, y,
+    );
+
+    const tmp = paillierMultiply(verifierPaillierPublicKey, C, x);
+    const { ciphertext: D_, nonce: rho } = paillierEncrypt(
+      verifierPaillierPublicKey, y,
+    );
+    const D = paillierAdd(verifierPaillierPublicKey, D_, tmp);
+
+    const pub: ZkAffpPublic = {
+      Kv: C,
+      Dv: D,
+      Fp: Y,
+      Xp: X,
+      prover: proverPaillierPublicKey,
+      verifier: verifierPaillierPublicKey,
+      aux: verifierPedersen,
+    };
+    const priv: ZkAffpPrivate = {
+      X: x,
+      Y: y,
+      S: rho,
+      Rx: rhoX,
+      R: rhoY,
+    };
+
+    const proof = zkAffpCreateProof(pub, priv);
+
+    const verified = zkAffpVerifyProof(proof, pub);
+
+    assert.equal(verified, true, "Proof verification failed");
+  });
+});
---
+++ src/lib/zk/affp.ts
@@ -0,0 +1,193 @@
+import { modMultiply, modPow } from "bigint-crypto-utils";
+
+import Fn from "../Fn.js";
+import { Hasher } from "../Hasher.js";
+import { isInIntervalLeps, isInIntervalLprimeEps, isValidModN } from "../arith.js";
+import {
+  PaillierPublicKey, paillierAdd, paillierEncryptWithNonce,
+  paillierMultiply, validateCiphertext,
+} from "../paillier.js";
+import { PedersenParameters, pedersenCommit, pedersenVerify } from "../pedersen.js";
+import {
+  sampleIntervalLN, sampleIntervalLeps, sampleIntervalLepsN,
+  sampleIntervalLprimeEps, sampleUnitModN,
+} from "../sample.js";
+
+export type ZkAffpPublic = {
+  Kv: bigint; // Ciphertext
+  Dv: bigint; // Ciphertext
+  Fp: bigint; // Ciphertext
+  Xp: bigint; // Ciphertext
+  prover: PaillierPublicKey;
+  verifier: PaillierPublicKey;
+  aux: PedersenParameters;
+};
+
+export type ZkAffpPrivate = {
+  X: bigint;
+  Y: bigint;
+  S: bigint;
+  Rx: bigint;
+  R: bigint;
+};
+
+export type ZkAffpCommitment = {
+  A: bigint; // Ciphertext
+  Bx: bigint; // Ciphertext
+  By: bigint; // Ciphertext
+  E: bigint;
+  S: bigint;
+  F: bigint;
+  T: bigint;
+};
+
+export type ZkAffpProof = {
+  commitment: ZkAffpCommitment;
+  Z1: bigint;
+  Z2: bigint;
+  Z3: bigint;
+  Z4: bigint;
+  W: bigint;
+  Wx: bigint;
+  Wy: bigint;
+};
+
+const isValid = (proof: ZkAffpProof, pub: ZkAffpPublic): boolean => {
+  if (!proof) { return false; }
+  if (!validateCiphertext(pub.verifier, proof.commitment.A)) { return false; }
+  if (!validateCiphertext(pub.prover, proof.commitment.Bx)) { return false; }
+  if (!validateCiphertext(pub.prover, proof.commitment.By)) { return false; }
+  if (!isValidModN(pub.prover.n, proof.Wx)) { return false; }
+  if (!isValidModN(pub.prover.n, proof.Wy)) { return false; }
+  if (!isValidModN(pub.verifier.n, proof.W)) { return false; }
+  return true;
+};
+
+export const zkAffpCreateProof = (
+  pub: ZkAffpPublic,
+  priv: ZkAffpPrivate,
+): ZkAffpProof => {
+  const N0 = pub.verifier.n;
+  const N1 = pub.prover.n;
+
+  const alpha = sampleIntervalLeps();
+  const beta = sampleIntervalLprimeEps();
+
+  const rho = sampleUnitModN(N0);
+  const rhoX = sampleUnitModN(N1);
+  const rhoY = sampleUnitModN(N1);
+
+  const gamma = sampleIntervalLepsN();
+  const m = sampleIntervalLN();
+  const delta = sampleIntervalLepsN();
+  const mu = sampleIntervalLN();
+
+  const cAlpha = paillierMultiply(pub.verifier, pub.Kv, alpha);
+  const A = paillierAdd(
+    pub.verifier,
+    paillierEncryptWithNonce(pub.verifier, beta, rho),
+    cAlpha,
+  );
+
+  const E = pedersenCommit(pub.aux, alpha, gamma);
+  const S = pedersenCommit(pub.aux, priv.X, m);
+  const F = pedersenCommit(pub.aux, beta, delta);
+  const T = pedersenCommit(pub.aux, priv.Y, mu);
+
+  const Bx = paillierEncryptWithNonce(pub.prover, alpha, rhoX);
+  const By = paillierEncryptWithNonce(pub.prover, beta, rhoY);
+
+  const commitment: ZkAffpCommitment = { A, Bx, By, E, S, F, T };
+
+  const e = challenge(pub, commitment);
+
+  const Z1 = priv.X * e + alpha;
+  const Z2 = priv.Y * e + beta;
+  const Z3 = m * e + gamma;
+  const Z4 = mu * e + delta;
+  const W = modMultiply([modPow(priv.S, e, N0), rho], N0);
+  const Wx = modMultiply([modPow(priv.Rx, e, N1), rhoX], N1);
+  const Wy = modMultiply([modPow(priv.R, e, N1), rhoY], N1);
+
+  return { commitment, Z1, Z2, Z3, Z4, W, Wx, Wy };
+};
+
+export const zkAffpVerifyProof = (proof: ZkAffpProof, pub: ZkAffpPublic): boolean => {
+  if (!isValid(proof, pub)) { return false; }
+
+  if (!isInIntervalLeps(proof.Z1)) { return false; }
+  if (!isInIntervalLprimeEps(proof.Z2)) { return false; }
+
+  const e = challenge(pub, proof.commitment);
+
+  {
+    const lhs = paillierAdd(
+      pub.verifier,
+      paillierEncryptWithNonce(pub.verifier, proof.Z2, proof.W),
+      paillierMultiply(pub.verifier, pub.Kv, proof.Z1),
+    );
+    const rhs = paillierAdd(
+      pub.verifier,
+      paillierMultiply(pub.verifier, pub.Dv, e),
+      proof.commitment.A,
+    );
+    if (lhs !== rhs) { return false; }
+  }
+
+  {
+    const lhs = paillierEncryptWithNonce(pub.prover, proof.Z1, proof.Wx);
+    const rhs = paillierAdd(
+      pub.prover,
+      paillierMultiply(pub.prover, pub.Xp, e),
+      proof.commitment.Bx,
+    );
+    if (lhs !== rhs) { return false; }
+  }
+
+  {
+    const lhs = paillierEncryptWithNonce(pub.prover, proof.Z2, proof.Wy);
+    const rhs = paillierAdd(
+      pub.prover,
+      paillierMultiply(pub.prover, pub.Fp, e),
+      proof.commitment.By,
+    );
+    if (lhs !== rhs) { return false; }
+  }
+
+  if (!pedersenVerify(
+    pub.aux, proof.Z1, proof.Z3, e, proof.commitment.E, proof.commitment.S
+  )) {
+    return false;
+  }
+
+  if (!pedersenVerify(
+    pub.aux, proof.Z2, proof.Z4, e, proof.commitment.F, proof.commitment.T
+  )) {
+    return false;
+  }
+
+  return true;
+};
+
+const challenge = (pub: ZkAffpPublic, commitment: ZkAffpCommitment): bigint => {
+  const bigHash = Hasher.create().updateMulti([
+    pub.aux,
+    pub.prover,
+    pub.verifier,
+    pub.Kv,
+    pub.Dv,
+    pub.Fp,
+    pub.Xp,
+    commitment.A,
+    commitment.Bx,
+    commitment.By,
+    commitment.E,
+    commitment.S,
+    commitment.F,
+    commitment.T,
+  ]).digestBigint();
+
+  const challenge = Fn.sub(bigHash, 2n ** 255n); // TODO
+
+  return challenge
+};
